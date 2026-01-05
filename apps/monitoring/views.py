1ba03c0ce54d1952723e79862085a736f8197430
Godfrey <ogembogodfrey75@gmail.com>
2026-01-05 20:17:47 +0300

    feat: Protect Prometheus metrics endpoint with staff_member_required

--- apps/monitoring/views.py
+++ apps/monitoring/views.py
@@ -2,12 +2,14 @@
 Views for the monitoring app.
 """
 
+from django.contrib.admin.views.decorators import staff_member_required
 from prometheus_client import generate_latest
 from django.http import HttpResponse
 
 from apps.monitoring.metrics import REGISTRY
 
 
+@staff_member_required
 def metrics_view(request):
     """
     Expose Prometheus metrics.
